5981a3bce575b1db9f94001a7a719a3a9ed504bd
ALSemenov <alhaos@gmail.com>
2025-09-19 12:05:46 +0500

    colors

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -4,8 +4,8 @@ import ScheduleSign from "../ScheduleSign/ScheduleSign.jsx";
 export default function Home() {
     return (
         <div className={styles.Home}>
-            <h1>Баня</h1>
-            <h2>Режим работы:</h2>
+            <h1 className={styles.Header}>Баня</h1>
+            <h2 className={styles.Caption}>Режим работы:</h2>
             <ScheduleSign/>
         </div>
     )
