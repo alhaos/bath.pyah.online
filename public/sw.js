4c99a5233e5e58329a279d73eb325a76e76ab094
ALSemenov <alhaos@gmail.com>
2025-09-28 20:25:41 +0500

    sync

--- public/sw.js
+++ public/sw.js
@@ -1,18 +1,3 @@
-async function networkFirst(request) {
-    try {
-        const networkResponse = await fetch(request);
-        if (networkResponse.ok) {
-            const cache = await caches.open("MyCache_1");
-            cache.put(request, networkResponse.clone());
-        }
-        return networkResponse;
-    } catch (error) {
-        const cachedResponse = await caches.match(request);
-        console.log(error)
-        return cachedResponse || Response.error();
-    }
-}
-
 self.addEventListener("fetch", (event) => {
-    event.respondWith(networkFirst(event.request));
+    event.respondWith(fetch(event.request));
 });
