061265e42f848b5881453ad8fcc1537b0f6692ed
ALSemenov <alhaos@gmail.com>
2025-09-28 14:00:43 +0500

    sync

--- src/Pages/ScheduleSign/ScheduleSign.jsx
+++ src/Pages/ScheduleSign/ScheduleSign.jsx
@@ -3,13 +3,13 @@ import styles from './ScheduleSign.module.css'
 export default function ScheduleSign() {
     return (
         <div className={styles.ScheduleSign}>
-            <table>
-                <p>24.09.2025 с 16.00 до 19.00 (мужской день)</p>
-                <p>25.09.2025 с 12.00 до 20-00 (мужской и женский день)</p>
-                <p>26.09.2025 с 12:00 до 20-00 (мужской и женский день)</p>
-                <p>27.09.2025 с 10:00 до 20-00 (мужской и женский день)</p>
-                <p>28.09.2025 с 10:00 до 20-00 (мужской и женский день)</p>
-            </table>
+            <div className={styles.Row}><span>Понедельник</span><span>выходной</span></div>
+            <div className={styles.Row}><span>Вторник</span><span>выходной</span></div>
+            <div className={styles.Row}><span>Среда</span><span>с 16:00 до 20-00</span></div>
+            <div className={styles.Row}><span>Четверг</span><span>с 16:00 до 20-00</span></div>
+            <div className={styles.Row}><span>Пятница</span><span>с 12:00 до 20-00</span></div>
+            <div className={styles.Row}><span>Суббота</span><span>с 10:00 до 20-00</span></div>
+            <div className={styles.Row}><span>Воскресенье</span><span>с 10:00 до 20-00</span></div>
         </div>
     )
 }
